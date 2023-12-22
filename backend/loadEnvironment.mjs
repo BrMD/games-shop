9b9f923e0be50301305a8eeffe39632a2760e662
BrMD <dorigon.matheus8@gmail.com>
2023-12-22 12:06:17 -0300

    backing again with loadenv

--- backend/loadEnvironment.mjs
+++ backend/loadEnvironment.mjs
@@ -1,20 +1,2 @@
-import SSM from "@aws-sdk/clients/ssm";
-
-const ssm = new SSM();
-
-export async function loadParameter(parameterName) {
-  try {
-    const { Parameter } = await ssm
-      .getParameter({
-        Name: parameterName,
-        WithDecryption: true,
-      })
-      .promise();
-
-    return Parameter?.Value ?? null;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-console.log(loadParameter("PORT"));
+import dotenv from "dotenv";
+dotenv.config();
